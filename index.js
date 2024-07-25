187545017788828fa36cec14f677ba90e509c146
lgar000 <laura.garcia-a@mail.escuelaing.edu.co>
2024-07-24 22:45:44 -0500

    exercise 3.8

--- index.js
+++ index.js
@@ -1,100 +1,107 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
     }
-]
+];
 
-app.use(express.json())
-app.use(morgan('tiny'));
 
-app.get('/api/persons',(request, response)=>{ 
-    response.json(persons)
-})
+app.use(express.json());
 
-app.get('/info', (request, response)=>{ 
-    const date=  new Date()
-    const numberPersons = persons.length
-    let mmessage = `<p>Ponebook as info for ${numberPersons} people</p><br><p>${date}</p>`;
-    response.send(mmessage)
-})
+morgan.token('body', function(req, res) {
+    return JSON.stringify(req.body);
+});
 
-app.get('/api/persons/:id',(request, response)=>{ 
-    const id= Number(request.params.id)
-    const person= persons.find(p=> p.id===id) 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '));
 
-    if(person){ 
-         response.json(person)
-    }else{ 
-         response.status(404).end()
+
+app.get('/api/persons', (request, response) => { 
+    response.json(persons);
+});
+
+app.get('/info', (request, response) => { 
+    const date = new Date();
+    const numberPersons = persons.length;
+    let message = `<p>Phonebook has info for ${numberPersons} people</p><br><p>${date}</p>`;
+    response.send(message);
+});
+
+app.get('/api/persons/:id', (request, response) => { 
+    const id = Number(request.params.id);
+    const person = persons.find(p => p.id === id); 
+
+    if (person) { 
+        response.json(person);
+    } else { 
+        response.status(404).end();
     }
-})
+});
 
-app.delete('/api/persons/:id', (request, response)=>{ 
-    const id= Number(request.params.id)
-    persons= persons.filter(p=> p.id!==id)
-    response.status(204).end()
-})
+app.delete('/api/persons/:id', (request, response) => { 
+    const id = Number(request.params.id);
+    persons = persons.filter(p => p.id !== id);
+    response.status(204).end();
+});
 
 const generateId = () => {
     let id;
     do {
         id = Math.floor(Math.random() * 1000000);
     } while (persons.find(p => p.id === id));
-    
     return id;
 };
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
 
-app.post('/api/persons', (request, response)=>{
-    const body = request.body
-   
-    if(persons.find(p=>p.name===body.name)  ){
+    if (persons.find(p => p.name === body.name)) {
         return response.status(400).json({
-            error: 'name  must be unique.' 
-        })
-    }if(!body.name){
+            error: 'name must be unique.'
+        });
+    }
+    if (!body.name) {
         return response.status(400).json({
-            error: 'name missin.' 
-        })
-    }if( !body.number){
+            error: 'name missing.'
+        });
+    }
+    if (!body.number) {
         return response.status(400).json({
-            error: 'number missin'
-        })
+            error: 'number missing'
+        });
     }
 
-    const person= {
+    const person = {
         name: body.name,
         number: body.number,
         id: generateId()
-    }
+    };
 
-    persons= persons.concat(person)
+    persons = persons.concat(person);
+    response.json(person);
+});
 
-    response.json(person)
-})
-const PORT= 3001
-app.listen(PORT, ()=>{ 
-    console.log(`Server running on port ${PORT}`)
-})
+
+const PORT = 3001;
+app.listen(PORT, () => { 
+    console.log(`Server running on port ${PORT}`);
+});
