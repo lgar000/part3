c22c7eb70b4dee92c680a2ce3e1636478e6f175a
lgar000 <laura.garcia-a@mail.escuelaing.edu.co>
2024-07-31 17:31:12 -0500

    exercises 3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length<3){
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://garla005:${password}@cluster0.ibh33fr.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+const personSchema= new mongoose.Schema({
+    name: String,
+    number: String,
+  }) 
+
+  const Person = mongoose.model('Person', personSchema)
+
+  mongoose.connect(url).then(()=>{
+    if(process.argv.length===5){
+        const name= process.argv[3]
+        const number= process.argv[4]
+        const person= new Person({
+            name: name,
+            number: number,
+          })
+        person.save().then(result=>{
+            console.log('added ' +name+ ' number '+ number+ ' to phonebook')
+            mongoose.connection.close()  
+    })
+    }else if(process.argv.length===3){
+        console.log('phonebook:')
+        Person.find({}).then(result=>{
+            result.forEach(person => {
+                console.log(person.name + " "+ person.number)
+            });
+            mongoose.connection.close()
+        })    
+    }    
+  })
